f38ea389e56432ca7ec774d755a33cfe7ecebd50
evertondewes <everton.dewes@gmail.com>
2019-03-30 10:16:17 -0300

    Um array int

--- src/aplicacaonova/AplicacaoNova.java
+++ src/aplicacaonova/AplicacaoNova.java
@@ -3,17 +3,27 @@ package aplicacaonova;
 import casa.Casa;
 import rh.*;
 
+import java.util.Scanner;
+
 public class AplicacaoNova {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] array = new int[4];
+        for(int i=0; i < 4; i++){
 
-        Pessoa everton;
+            String linha;
 
-        everton = new Pessoa();
+            linha = s.nextLine();
 
-        everton.setAltura(-10);
-
-        everton.setNome("Everton", "Sobrenome");
+            array[i] = Integer.parseInt(linha);
+        }
+        for (int i: array){
+            System.out.println(i);
+        }
+        for(int i=0; i < 4; i++){
+            System.out.println(array[i]);
+        }
 
     }
 
--- src/rh/Pessoa.java
+++ src/rh/Pessoa.java
@@ -2,7 +2,7 @@ package rh;
 
 public class Pessoa {
 
-    private String nome;
+    String nome;
     private String sobrenome;
     public String dataDeNascimento;
     public String sexo;
@@ -11,9 +11,20 @@ public class Pessoa {
 
     public static int totalPessoas = 0;
 
-    public void setAltura(float altura){
-        this.altura = altura;
+    public void setSexo(String sexo){
+        if(sexo.equals("m") || sexo.equals("f") || sexo.equals("o") ) {
+            this.sexo = sexo;
+        } else {
+            System.out.println("Erro, sexo inválido");
+        }
+    }
 
+    public void setAltura(float altura){
+        if(altura > 0.1) {
+            this.altura = altura;
+        } else {
+            System.out.println("Altura Inválida");
+        }
     }
 
     public boolean setNome(String nome) {
