fe6cdee85d2f460a6e200e117f1e126b0ff52d11
evertondewes <everton.dewes@gmail.com>
2019-04-04 23:10:43 -0300

    exibr e remove um registro

--- src/aplicacaonova/AplicacaoNova.java
+++ src/aplicacaonova/AplicacaoNova.java
@@ -1,18 +1,24 @@
 package aplicacaonova;
+
+import com.sun.deploy.util.StringUtils;
 import rh.Pessoa;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class AplicacaoNova {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         ArrayList<Pessoa> listagemPessoas = new ArrayList<Pessoa>();
         String opcao = "s";
+
         do {
             System.out.println("Deseja adicionar:");
             opcao = s.nextLine();
-            if(opcao.equals("s")) {
+
+            if (opcao.equals("s")) {
                 Pessoa p = new Pessoa();
                 System.out.println("Entre com o nome:");
                 p.setNome(s.nextLine());
@@ -21,16 +27,31 @@ public class AplicacaoNova {
                 System.out.println("Entre com o Sexo");
                 p.setSexo(s.nextLine());
                 listagemPessoas.add(p);
+            } else {
+                int indice = Integer.parseInt(opcao);
+
+                if(indice < listagemPessoas.size()) {
+
+                    Pessoa p = listagemPessoas.get(indice);
+
+                    System.out.println("Nome: " + p.getNome());
+                    System.out.println("Altura: " + p.getAltura());
+                    System.out.println("Sexo: " + p.getSexo());
+
+                    listagemPessoas.remove(indice);
+                } else {
+                    System.out.println("Tentando remover fora do intervalo");
+                }
             }
         } while (opcao.equals("s"));
 
-        for (Pessoa p: listagemPessoas){
+        for (Pessoa p : listagemPessoas) {
             System.out.println(p.getNome());
             System.out.println(p.getAltura());
             System.out.println(p.getSexo());
         }
 
-        for(int i=0; i < listagemPessoas.size(); i++){
+        for (int i = 0; i < listagemPessoas.size(); i++) {
             System.out.println(listagemPessoas.get(i).getNome());
             System.out.println(listagemPessoas.get(i).getAltura());
             System.out.println(listagemPessoas.get(i).getSexo());
